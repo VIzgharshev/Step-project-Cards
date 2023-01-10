b6931513c5fefa5c29f3ea1f5849e3cdaef2f6cd
Vladymyr Izgharshev <izgharshev.vladymyr@gmail.com>
2023-01-10 20:00:32 +0200

    adaptive variants filter and header

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,3 +1,4 @@
+//large variables
 const textInput = document.querySelector('#filter-text');
 const searchBtn = document.querySelector('#filter-serch-btn');
 const openBtn = document.querySelector('#filter-open-btn');
@@ -6,6 +7,25 @@ const highBtn = document.querySelector('#filter-high-btn');
 const normalBtn = document.querySelector('#filter-normal-btn');
 const lowBtn = document.querySelector('#filter-low-btn');
 
+//small variables
+const textInputS = document.querySelector('#filter-text-s');
+const searchBtnS = document.querySelector('#filter-serch-btn-s');
+const openBtnS = document.querySelector('#filter-open-btn-s');
+const doneBtnS = document.querySelector('#filter-done-btn-s');
+const highBtnS = document.querySelector('#filter-high-btn-s');
+const normalBtnS = document.querySelector('#filter-normal-btn-s');
+const lowBtnS = document.querySelector('#filter-low-btn-s');
+
+//small filter function usege
+openBtnS.onclick = statusBtnsS;
+doneBtnS.onclick = statusBtnsS;
+highBtnS.onclick = urgencyBtnsS;
+normalBtnS.onclick = urgencyBtnsS;
+lowBtnS.onclick = urgencyBtnsS;
+textInputS.onkeyup = test;
+searchBtnS.onclick = test2;
+
+//large filter function usege
 openBtn.onclick = statusBtns;
 doneBtn.onclick = statusBtns;
 highBtn.onclick = urgencyBtns;
@@ -14,17 +34,41 @@ lowBtn.onclick = urgencyBtns;
 textInput.onkeyup = test;
 searchBtn.onclick = test2;
 
-
 function test2() {
-   alert('searching...')
-};
+  alert('searching...');
+}
 
 function test(e) {
-   if (e.keyCode === 13) {
-      alert('enter works!!! searching...');
-   }
-};
+  if (e.keyCode === 13) {
+    alert('enter works!!! searching...');
+  }
+}
 
+//------------small filter function-----------
+function statusBtnsS(e) {
+  e.target.classList.add('filter-s__btn--active');
+  if (e.target === openBtnS) {
+    doneBtnS.classList.remove('filter-s__btn--active');
+  } else {
+    openBtnS.classList.remove('filter-s__btn--active');
+  }
+}
+
+function urgencyBtnsS(e) {
+  e.target.classList.add('filter-s__btn--active');
+  if (e.target === highBtnS) {
+    normalBtnS.classList.remove('filter-s__btn--active');
+    lowBtnS.classList.remove('filter-s__btn--active');
+  } else if (e.target === normalBtnS) {
+    highBtnS.classList.remove('filter-s__btn--active');
+    lowBtnS.classList.remove('filter-s__btn--active');
+  } else {
+    highBtnS.classList.remove('filter-s__btn--active');
+    normalBtnS.classList.remove('filter-s__btn--active');
+  }
+}
+
+//------------large filter function-----------
 function statusBtns(e) {
   e.target.classList.add('filter__btn--active');
   if (e.target === openBtn) {
--- src/js/header.js
+++ src/js/header.js
@@ -40,7 +40,7 @@ async function modalDoneBtn() {
         emailText.style.color = 'red';
         emailText.innerHTML = 'Incorrect username or password!';
       } else {
-        headerText.innerHTML = `Вітаємо, ви зайшли в акаунт ${email}!`;
+        headerText.innerHTML = `Вітаємо, ${email}!`;
         modalLogin.classList.add('invisible');
         headerBtnLogin.classList.add('invisible');
         headerBtns.classList.remove('invisible');
